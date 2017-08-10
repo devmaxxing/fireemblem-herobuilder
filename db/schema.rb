80c072b12e340d979b0237d44bfa0479f8c38216
Oliver Fei <ofei006@uottawa.ca>
2017-08-10 20:32:22 +0000

    Add devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170628025730) do
+ActiveRecord::Schema.define(version: 20170804145542) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,11 @@ ActiveRecord::Schema.define(version: 20170628025730) do
     t.integer "base_defense"
     t.integer "base_speed"
     t.integer "base_resistance"
+    t.integer "max_attack"
+    t.integer "max_defense"
+    t.integer "max_health"
+    t.integer "max_resistance"
+    t.integer "max_speed"
   end
 
   create_table "skills", force: :cascade do |t|
@@ -65,6 +70,24 @@ ActiveRecord::Schema.define(version: 20170628025730) do
 
   create_table "specials", force: :cascade do |t|
     t.integer "cooldown"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,     null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+    t.boolean  "admin",                  default: false
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
   create_table "weapons", force: :cascade do |t|
