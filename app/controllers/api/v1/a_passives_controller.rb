23dbcf5aae23c5fe0310efa68706fdd02e8fe90b
Oliver Fei <ofei006@uottawa.ca>
2017-08-18 20:15:05 +0000

    bugfix

--- app/controllers/api/v1/a_passives_controller.rb
+++ app/controllers/api/v1/a_passives_controller.rb
@@ -1,6 +1,6 @@
 module Api
     module V1
-        class APassivesController < Api::SkillsController
+        class APassivesController < SkillsController
             private
                 def a_passive_params
                     params.require(:skill).permit(:effect_health, :effect_attack, :effect_speed, :effect_defense, :effect_health)
